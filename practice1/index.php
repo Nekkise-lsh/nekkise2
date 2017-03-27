b8eaa322462fbe20d9e932156774b1a5aefa50ec
Nekisse <lsh891224@gmail.com>
2017-03-27 17:02:01 +0900

    부록 끝

--- practice1/index.php
+++ practice1/index.php
@@ -1,89 +1,50 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'llllll');
-mysqli_select_db($conn, 'opentutorials2');
- ?>
-
+require_once('conn.php');
+?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <style media="screen">
-    body{
-      margin: 0;
-    }
-    body.black{
-      background-color: black;
-      color:white;
-    }
-      header{
-        border-bottom: 1px solid gray;
-        padding-left: 70px;
-      }
-      nav {
-        border-right: 1px solid gray;
-        width:200px;
-        height: 700px;
-        float: left;
-      }
-      nav ol{
-        margin: 0;
-        padding:20px;
-        list-style: none;
-      }
-      $content{
-        padding-left: 20px;
-        float: left;
-        width: 400px;
-      }
-    </style>
-  </head>
-  <body id="body">
-    <header>
-      <h1><a href="index.php">생활코딩 JavaScript</h1>
-    </header>
-    <nav>
-      <ol>
-
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+    </head>
+    <body id="body">
+        <header>
+            <h1><a href="index.php">생활코딩 JavaScript</a></h1>
+        </header>
+        <nav>
+            <ol>
 <?php
-$sql  = "SELECT * FROM `topic`";
+$sql = "SELECT * FROM `topic`";
 $result = mysqli_query($conn, $sql);
-while ($row = mysqli_fetch_assoc($result)) {
-   echo '<li><a href="index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>';
-}
- ?>
-
-      </ol>
-    </nav>
-    <div="content">
-      <article>
-<?php
-if (empty($_GET['id'])){
-   echo "Welcome";
-} else {
-  $id = mysqli_real_escape_string($conn, $_GET['id']);
-  $sql  = "SELECT topic.id, topic.title, topic.description, user.name, topic.created FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id =".$id;
-  $result = mysqli_query($conn, $sql);
-  $row = mysqli_fetch_assoc($result);
-  ?>
-  <h2><?=htmlspecialchars($row['title'])?></h2>
-  <div><?=htmlspecialchars($row['created'])?> | <?=htmlspecialchars($row['name'])?></div>
-  <div><?=htmlspecialchars($row['description'])?></div>
-  <?php
+while($row = mysqli_fetch_assoc($result)){
+    echo '<li><a href="index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>';
 }
 ?>
+            </ol>
+        </nav>
+        <div id="content">
+            <article>
+    <?php
+    if(empty($_GET['id'])){
+        echo "Welcome";
+    } else {
+        $id = mysqli_real_escape_string($conn, $_GET['id']);
+        $sql = "SELECT topic.id, topic.title, topic.description, user.name, topic.created FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$id;
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+    ?>
+        <h2><?=htmlspecialchars($row['title'])?></h2>
+        <div><?=htmlspecialchars($row['created'])?> | <?=htmlspecialchars($row['name'])?></div>
+        <div><?=htmlspecialchars($row['description'])?></div>
+    <?php
+    }
+    ?>
 
-      </article>
-      <input type="button" name="name" value="White" onclick="document.getElementById('body').className=''">
-      <input type="button" name="name" value="Black" onclick="document.getElementById('body').className='black'">
-      <a href="write.php">쓰기</a>
-   </div>
-  </body>
+            </article>
+            <input type="button" name="name" value="White" onclick="document.getElementById('body').className=''">
+            <input type="button" name="name" value="Black" onclick="document.getElementById('body').className='black'">
+            <a href="write.php">쓰기</a>
+        </div>
+    </body>
 </html>
-
-
-
-<?php
-
-
- ?>
--- practice1/write.php
+++ practice1/write.php
@@ -1,91 +1,51 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'llllll');
-mysqli_select_db($conn, 'opentutorials2');
- ?>
-
+require_once('conn.php');
+?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <style media="screen">
-    body{
-      margin: 0;
-    }
-    body.black{
-      background-color: black;
-      color:white;
-    }
-      header{
-        border-bottom: 1px solid gray;
-        padding-left: 70px;
-      }
-      nav {
-        border-right: 1px solid gray;
-        width:200px;
-        height: 700px;
-        float: left;
-      }
-      nav ol{
-        margin: 0;
-        padding:20px;
-        list-style: none;
-      }
-      $content{
-        padding-left: 20px;
-        float: left;
-        width: 400px;
-      }
-    </style>
-  </head>
-  <body id="body">
-    <header>
-      <h1><a href="index.php">생활코딩 JavaScript</h1>
-    </header>
-    <nav>
-      <ol>
-
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+    </head>
+    <body id="body">
+        <header>
+            <h1><a href="index.php">생활코딩 JavaScript</a></h1>
+        </header>
+        <nav>
+            <ol>
 <?php
-$sql  = "SELECT * FROM `topic`";
+$sql = "SELECT * FROM `topic`";
 $result = mysqli_query($conn, $sql);
-while ($row = mysqli_fetch_assoc($result)) {
-   echo '<li><a href="index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>';
+while($row = mysqli_fetch_assoc($result)){
+    echo '<li><a href="index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>';
 }
- ?>
-
-      </ol>
-    </nav>
-    <div="content">
-      <article>
-        <form class="" action="process.php" method="post">
-          <p>
-            <label for="title"> 제목 :</label>
-            <input id="title" type="text" name="title">
-          </p>
-          <p>
-            <label for="author">저자 : </label>
-            <input id="author" type="text" name="author" value="">
-          </p>
-          <p>
-            <label for="description">본문 :</label>
-            <textarea id="description" name="description" rows="8" cols="40"></textarea>
-          </p>
-          <p>
-            <input type="submit" value="전송">
-          </p>
-        </form>
-
-      </article>
-      <input type="button" name="name" value="White" onclick="document.getElementById('body').className=''">
-      <input type="button" name="name" value="Black" onclick="document.getElementById('body').className='black'">
-      <a href="write.php">쓰기</a>
-   </div>
-  </body>
+?>
+            </ol>
+        </nav>
+        <div id="content">
+            <article>
+                <form class="" action="process.php" method="post">
+                    <p>
+                        <label for="title">제목 :</label>
+                        <input id="title" type="text" name="title">
+                    </p>
+                    <p>
+                        <label for="author">저자 : </label>
+                        <input id="author" type="text" name="author" value="">
+                    </p>
+                    <p>
+                        <label for="description">본문 :</label>
+                        <textarea id="description" name="description" rows="8" cols="40"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit"  value="전송">
+                    </p>
+                </form>
+            </article>
+            <input type="button" name="name" value="White" onclick="document.getElementById('body').className=''">
+            <input type="button" name="name" value="Black" onclick="document.getElementById('body').className='black'">
+            <a href="write.php">쓰기</a>
+        </div>
+    </body>
 </html>
-
-
-
-<?php
-
-
- ?>
